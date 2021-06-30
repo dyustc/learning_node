4b25da278a30aec425e7b46239cd7d4334021ad5
Dai, Yi <daiyi.thin@bytedance.com>
2021-06-30 15:37:49 +0800

    feat: introduce callback spaghetti
    Change-Id: If2eae538f3c4a0c7687f0f1bdbcf04c38ac16250

--- example-code/e2_10.js
+++ example-code/e2_10.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var fs = require('fs');
 
 var writeStream = fs.createWriteStream('./log.txt',
     {'flags' : 'a',
              'encoding' : 'utf8',
-             'mode' : 0666});
+             'mode' : 0o666});
+
 // TODO: on的event 也是指定的
 writeStream.on('open', function () {
     // get list of files
@@ -12,25 +15,41 @@ writeStream.on('open', function () {
             console.error(err);
             // console.log(err.message);
         } else {
+            var counter = 0;
             files.forEach(function (name) {
-                fs.readFile('./data/' + name, 'utf8', function (err, data) {
-                    if (err) {
-                        console.error(err.message);
-                    } else {
-                        var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
-                        fs.writeFile('./data/' + name, adjData, function (err) {
-                            if (err) {
-                                console.error(err.message);
-                            } else {
-                                // log write
-                                writeStream.write('changed ' + name + '\n', 'utf8', function (err) {
-                                    if (err) console.log(err.message);
-                                });
-                            }
-                        });
+                fs.stat('./data/' + name, function (err, stats) {
+                    if (err) return err;
+                    if (!stats.isFile()) {
+                        counter++;
+                        return;
                     }
+                    fs.readFile('./data/' + name, 'utf8', function (err, data) {
+                        if (err) {
+                            // console.error(err.message);
+                            console.error(err);
+                        } else {
+                            var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
+                            fs.writeFile('./data/' + name, adjData, function (err) {
+                                if (err) {
+                                    console.error(err.message);
+                                } else {
+                                    // log write
+                                    counter++;
+                                    writeStream.write('changed ' + name + '\n', 'utf8', function (err) {
+                                        if (err) console.log(err.message);
+                                    });
+                                    if (counter === files.length) {
+                                        writeStream.write('all files touched\n', 'utf8', function (err) {
+                                            if (err) console.log(err.message);
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    });
                 });
             });
+            console.log('All done');
         }
     });
 });
